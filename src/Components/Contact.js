b91f6198670c9360aeda4456a4e809ab6c222ce2
Antho Stasolla <anthostasoll89@gmail.com>
2023-04-21 10:49:32 +0200

    added form contact

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -9,23 +9,37 @@ const Contact = () => {
         <h4 className="text-about1 mb-5">
           -Compila il form o inviami una mail - anthostasoll89@gmail.com -
         </h4>
-        <form className="container">
+        <form
+          className="container"
+          action="https://getform.io/f/52b40f2e-4e64-40af-9fd3-95e4afdeddec"
+          method="POST"
+        >
           <div className="form-outline mb-4">
-            <input type="text" id="name" className="form-control" />
+            <input type="text" id="name" name="name" className="form-control" />
             <label className="form-label" htmlFor="name">
               NOME
             </label>
           </div>
 
           <div className="form-outline mb-4">
-            <input type="email" id="email" className="form-control" />
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className="form-control"
+            />
             <label className="form-label" htmlFor="email">
               EMAIL
             </label>
           </div>
 
           <div className="form-outline mb-4">
-            <textarea className="form-control" id="text" rows="6"></textarea>
+            <textarea
+              className="form-control"
+              name="textarea"
+              id="text"
+              rows="6"
+            ></textarea>
             <label className="form-label" htmlFor="text">
               SCRIVI UN MESSAGGIO
             </label>
